615c138109af665e51d47a9074cdf643ec00b797
juza-w <juza.watanabe@student.manchester.ac.uk>
2026-04-16 17:54:36 +0000

    2nd trial

--- VHDL_Labs/Tutorial/circuit.vhd
+++ VHDL_Labs/Tutorial/circuit.vhd
@@ -1,16 +1,15 @@
 library ieee;
 use ieee.std_logic_1164.all;
 entity circuit is
-port (
-a, b, enable : in std_logic;
-y: out std_logic
-);
+  port (
+    a, b, enable : in std_logic;
+    y            : out std_logic
+  );
 end circuit;
 
-architecture rtl of circuit is 
+architecture rtl of circuit is
   signal q : std_logic;
 begin
-  q <= a and b;
+  q <= a nand b;
   y <= not q when enable = '0' else 'Z';
 end architecture;
-
