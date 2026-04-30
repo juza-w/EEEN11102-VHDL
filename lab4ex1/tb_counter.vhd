934cea076ffcfbd5cfc9d4b5fcb81ba2cb43cc77
juza-w <juza.watanabe@student.manchester.ac.uk>
2026-04-30 06:20:46 +0000

    lab4.1

--- lab4ex1/tb_counter.vhd
+++ lab4ex1/tb_counter.vhd
@@ -1,7 +1,3 @@
--- Digital Electronics VHDL Laboratory, Univeristy of  Manchester
--- VHDL Test Bench Created from source file counter.vhd
--- Lab #4 v2026
-
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -10,11 +6,10 @@ entity tb_counter is
 end;
 
 architecture testbench of tb_counter is
-  -- Ports
   signal preset : std_logic;
-  signal clk : std_logic;
-  signal up : std_logic;
-  signal count : std_logic_vector (7 downto 0);
+  signal clk    : std_logic;
+  signal up     : std_logic;
+  signal count  : std_logic_vector (7 downto 0);
 
   component counter
     port (
@@ -35,83 +30,77 @@ begin
     count  => count
   );
 
-	clock_gen; process
-	begin
-     clk <= '0'; wait for 500 ns;
-     clk <= '1'; wait for 500 ns;
-  end process; --clock_gen
+  clock_gen: process
+  begin
+    clk <= '0'; wait for 500 ns;
+    clk <= '1'; wait for 500 ns;
+  end process;
 
-   stimulus_gen: process
-   begin
-	  preset <= '1';
-	  up <= '1';
-	  wait for 1.4 us;
-	  preset <= '0';
-	  wait for 4 us;
-	  up <= '0';
-	  wait for 2.2 us;
-	  preset <= '1';	  
-	  
-	  wait; 
-   end process; 
-   
- 
-   monitor: process
-   begin
-	 	wait for 1.2 us;
-		assert (count = "11111111")
-		 report "ERROR - counter di not preset"
-		 severity FAILURE;
-	
-		wait for 500 ns;
-		assert (count = "00000000")
-			report "ERROR - counter not rising edge triggered"
-			severity FIALURE;
+  stimulus_gen: process
+  begin
+    preset <= '1';
+    up     <= '1';
+    wait for 1.4 us;
+    preset <= '0';
+    wait for 4 us;
+    up     <= '0';
+    wait for 2.2 us;
+    preset <= '1';
+    wait;
+  end process;
 
-		wait for 500 ns;
-		assert (count = "00000000")
-			report "ERROR - should be counting up"
-			severity FAILURE;
-
-		wait for 1 us;
-		assert (count = "00000001")
-			report "ERROR - should be counting up"
-			severity FAILURE;
-		
-		wait for 1 us;
-		assert (count = "00000010")
-			report "ERROR - should be counting up"
+  monitor: process
+  begin
+    wait for 1.2 us;
+    assert (count = "11111111")
+      report "ERROR - counter did not preset"
       severity FAILURE;
 
-		wait for 1 us;    
-    assert (count = "00000011") 
-        report "ERROR - should be counting up" 
-        severity FAILURE;
+    wait for 500 ns;
+    assert (count = "00000000")
+      report "ERROR - counter not rising edge triggered"
+      severity FAILURE;
 
-	  wait for 500 ns;      
-    assert (count = "00000010")
-        report "ERROR - counter not counting down"
-        severity FAILURE;
+    wait for 500 ns;
+    assert (count = "00000000")
+      report "ERROR - should be counting up"
+      severity FAILURE;
 
-    -- check another down step
-    wait for 1 us;      
+    wait for 1 us;
     assert (count = "00000001")
-        report "ERROR - counter not counting down"
-	      severity FAILURE;
-	
-	 wait for 1300 ns;       
+      report "ERROR - should be counting up"
+      severity FAILURE;
+
+    wait for 1 us;
+    assert (count = "00000010")
+      report "ERROR - should be counting up"
+      severity FAILURE;
+
+    wait for 1 us;
+    assert (count = "00000011")
+      report "ERROR - should be counting up"
+      severity FAILURE;
+
+    wait for 500 ns;
+    assert (count = "00000010")
+      report "ERROR - counter not counting down"
+      severity FAILURE;
+
+    wait for 1 us;
+    assert (count = "00000001")
+      report "ERROR - counter not counting down"
+      severity FAILURE;
+
+    wait for 1300 ns;
     assert (count = "11111111")
-        report "ERROR - preset is not asynchronous"
-        severity FAILURE;
+      report "ERROR - preset is not asynchronous"
+      severity FAILURE;
 
-    assert FALSE 
-        report "OK. All tests passed. End of simulation." 
-        severity NOTE;
+    assert FALSE
+      report "OK. All tests passed. End of simulation."
+      severity NOTE;
 
-		wait;
-end monitor;
-
-
-
+    wait;
+  end process;
 
 end;
