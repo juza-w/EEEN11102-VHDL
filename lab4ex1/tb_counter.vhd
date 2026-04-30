04e80c10338ad915493c4679651550d29b9c9c8c
juza-w <juza.watanabe@student.manchester.ac.uk>
2026-04-30 07:12:11 +0100

    lab4.1 fix

--- lab4ex1/tb_counter.vhd
+++ lab4ex1/tb_counter.vhd
@@ -34,80 +34,84 @@ begin
     up     => up,
     count  => count
   );
+
+	clock_gen; process
+	begin
      clk <= '0'; wait for 500 ns;
      clk <= '1'; wait for 500 ns;
-   end process; --clock_gen
+  end process; --clock_gen
 
-   -----------------------------------
-   -- other stimuli
-   -----------------------------------
    stimulus_gen: process
    begin
-	  -- preset the counter to initialise
 	  preset <= '1';
-	  -- set it to count up
 	  up <= '1';
 	  wait for 1.4 us;
-     -- stop presetting, the counter should count up from now on...
 	  preset <= '0';
 	  wait for 4 us;
-     -- then try counting down
 	  up <= '0';
 	  wait for 2.2 us;
-     -- preset, should be asynchronous
 	  preset <= '1';	  
 	  
-	  wait; -- wait forever (makes sure process does not loop)
-   end process; -- stimulus_gen
+	  wait; 
+   end process; 
    
-   -----------------------------------
-   -- monitor the outputs
-   -----------------------------------
+ 
    monitor: process
    begin
-	  -- first, wait for some time - the initialisation should be ignored in the testbench.
-     wait for 1.2 us;
-	  -- the counter should preset after first clock edge
-	  assert (count = "11111111") report "ERROR - counter did not preset!" severity FAILURE;
-	  -- the counter should count up, first it will overflow from 255 to 0
-	  wait for 1 us;
-	  assert (count = "00000000") report "ERROR - should be counting up" severity FAILURE;
-	  -- counter is counting up for next 3 cycles
-	  wait for 1 us;
-	  assert (count = "00000001") report "ERROR - should be counting up" severity FAILURE;
-	  wait for 1 us;
-	  assert (count = "00000010") report "ERROR - should be counting up" severity FAILURE;
-	  wait for 1 us;
-	  assert (count = "00000011") report "ERROR - should be counting up" severity FAILURE;
-	  
-	  -- verify counting up continues correctly
-	  wait for 1 us;
-	  assert (count = "00000100") report "ERROR - should be counting up to 4" severity FAILURE;
-	  wait for 1 us;
-	  assert (count = "00000101") report "ERROR - should be counting up to 5" severity FAILURE;
-	  wait for 1 us;
-	  assert (count = "00000110") report "ERROR - should be counting up to 6" severity FAILURE;
-	  wait for 1 us;
-	  assert (count = "00000111") report "ERROR - should be counting up to 7" severity FAILURE;
-	   
-	  wait for 2 us;
-	  -- now should be counting down
-	  assert (count < "00001000") report "ERROR - should be counting down now" severity FAILURE;
-	  wait for 1 us;
-	  -- verify it continues counting down
-	  assert (count < "00000110") report "ERROR - should continue counting down" severity FAILURE;
-	  wait for 1 us;
-	  -- verify it continues counting down
-	  assert (count < "00000100") report "ERROR - should continue counting down further" severity FAILURE;
-	  
-	  wait for 1.2 us;
-	  assert (count = "11111111") report "ERROR - counter did not preset at the end!" severity FAILURE;
-	  -- end of tests
-	  	  
-	  -- the following line will generate an end of simulation message. This can be used make sure all test were run. 
-	  assert FALSE report "All tests OK. End of simulation." severity NOTE;
-	  wait; -- make sure the process stops here and doesnt loop.
-   
-	end process; -- monitor
+	 	wait for 1.2 us;
+		assert (count = "11111111")
+		 report "ERROR - counter di not preset"
+		 severity FAILURE;
+	
+		wait for 500 ns;
+		assert (count = "00000000")
+			report "ERROR - counter not rising edge triggered"
+			severity FIALURE;
+
+		wait for 500 ns;
+		assert (count = "00000000")
+			report "ERROR - should be counting up"
+			severity FAILURE;
+
+		wait for 1 us;
+		assert (count = "00000001")
+			report "ERROR - should be counting up"
+			severity FAILURE;
+		
+		wait for 1 us;
+		assert (count = "00000010")
+			report "ERROR - should be counting up"
+      severity FAILURE;
+
+		wait for 1 us;    
+    assert (count = "00000011") 
+        report "ERROR - should be counting up" 
+        severity FAILURE;
+
+	  wait for 500 ns;      
+    assert (count = "00000010")
+        report "ERROR - counter not counting down"
+        severity FAILURE;
+
+    -- check another down step
+    wait for 1 us;      
+    assert (count = "00000001")
+        report "ERROR - counter not counting down"
+	      severity FAILURE;
+	
+	 wait for 1300 ns;       
+    assert (count = "11111111")
+        report "ERROR - preset is not asynchronous"
+        severity FAILURE;
+
+    assert FALSE 
+        report "OK. All tests passed. End of simulation." 
+        severity NOTE;
+
+		wait;
+end monitor;
+
+
+
 
 end;
